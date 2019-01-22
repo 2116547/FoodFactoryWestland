44981220eba5686279fff2c17ad3e496273c3c82
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2019-01-22 17:22:08 +0100

    background troubleshoot

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/FoodFactoryWestland')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/FoodFactoryWestland/index.html'));

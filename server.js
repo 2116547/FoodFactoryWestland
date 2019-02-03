4172d98b118adb3a45e841153ff4e7732165ed6e
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2019-02-03 23:55:04 +0100

    troubleshoot

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const app = express();
 app.use(cors)
 
 
-app.use(express.static(__dirname, 'dist/FoodFactoryWestland'));
+app.use(express.static(path.join(__dirname, 'dist/FoodFactoryWestland')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/FoodFactoryWestland/index.html'));
 });
 
 const port = process.env.PORT || 8080;
-app.set('port', port);
+//app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log('running'));

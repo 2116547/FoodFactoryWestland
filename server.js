6aff00d4535cef25ccc086ef7ba16896097319b3
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2019-02-03 23:50:30 +0100

    troubleshoot

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(cors)
 
-app.use(express.static(path.join(__dirname, 'dist/FoodFactoryWestland')));
+
+app.use(express.static(__dirname, 'dist/FoodFactoryWestland'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/FoodFactoryWestland/index.html'));
